0965760aab4745a7de04ce1cc187798646bc847f
Mohd Mustafa Ahmed <mustafamohdvvip@gmail.com>
2025-04-22 23:48:03 +0530

    Add files via upload

---
+++ op.java
@@ -0,0 +1,11 @@
+public class op {
+
+    public static void main(String[] args) {
+       /* int x=Integer.parseInt(args[0]);
+        int y=Integer.parseInt(args[1]);
+        System.out.println("the sum is"+(x+y));
+
+        //System.out.println("hello world");*/ 
+        
+    }
+}
